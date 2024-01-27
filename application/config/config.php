baae8fe325faea8ff27381c5e1993d7bd336c001
RizalFakhriansy4h <22081000002@student.unmer.ac.id>
2024-01-28 01:35:00 +0700

    ubah untuk hosting

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci3rumahsakit/';
+// $config['base_url'] = 'http://localhost/ci3rumahsakit/';
+$config['base_url'] = 'https://miftahul.xyz/';
 
 /*
 |--------------------------------------------------------------------------
